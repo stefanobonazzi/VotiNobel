074beb733a21869fe473f0916bedfb61b3121bf6
stefanobonazzi <stefanobonazzi@mbp-di-stefano.lan>
2022-04-14 12:30:42 +0200

    Salvataggio pre vacanze Pasquali

--- src/main/java/it/polito/tdp/nobel/db/DBConnect.java
+++ src/main/java/it/polito/tdp/nobel/db/DBConnect.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnect {
 
-	static private final String jdbcUrl = "jdbc:mysql://localhost/esamitriennale?user=root&password=rootroot";
+	static private final String jdbcUrl = "jdbc:mysql://localhost/esamitriennale?user=stefanobonazzi&password=psw2810";
 
 	public static Connection getConnection() {
 
--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,15 +1,108 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import it.polito.tdp.nobel.db.EsameDAO;
 
 public class Model {
 
+	private List<Esame> esami;
+	private Set<Esame> best;
+	private double bestMedia;
 	
-	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
-		System.out.println("TODO!");
-		return null;	
+	public Model() {
+		EsameDAO dao = new EsameDAO();
+		this.esami = dao.getTuttiEsami();
 	}
 
+
+	public Set<Esame> calcolaSottoinsiemeEsami(int m) {
+		//Ripristino soluzione migliore
+		best = new HashSet<Esame>();
+		bestMedia = 0.0;
+		
+		Set<Esame> parziale = new HashSet<Esame>();
+		
+		cerca2(parziale, 0, m);
+		
+		return best;	
+	}
+	
+	/**
+	 * Questo algoritmo ha complessità n-fattoriale (N!)
+	 * @param parziale
+	 * @param l
+	 * @param m
+	 */
+	private void cerca1(Set<Esame> parziale, int l, int m) {
+		// Controllare i casi terminali
+		int sommaCrediti = sommaCrediti(parziale);
+		if(sommaCrediti > m)
+			return;
+		
+		if(sommaCrediti == m) {
+			//Soluzione valida! Controlliamo se è la migliore
+			double mediaVoti = calcolaMedia(parziale);
+			if(mediaVoti > this.bestMedia) {
+				best = new HashSet<Esame>(parziale);
+				bestMedia = mediaVoti;
+			}
+			
+			return;
+		}
+		
+		//Sicuramente i crediti sono < m se siamo in questo punto
+		if(l == esami.size())
+			return;
+		
+		//Generiamo i sotto-problemi
+		for(Esame e: esami) {
+			if(! parziale.contains(e)) {
+				parziale.add(e);
+				cerca1(parziale, l+1, m);
+				parziale.remove(e);
+			}
+		}
+		
+	}
+
+	/**
+	 * Questo è l'algoritmo più veloce
+	 * @param parziale
+	 * @param l
+	 * @param m
+	 */
+	private void cerca2(Set<Esame> parziale, int l, int m) {
+		// Controllare i casi terminali
+		int sommaCrediti = sommaCrediti(parziale);
+		if(sommaCrediti > m)
+			return;
+		
+		if(sommaCrediti == m) {
+			//Soluzione valida! Controlliamo se è la migliore
+			double mediaVoti = calcolaMedia(parziale);
+			if(mediaVoti > this.bestMedia) {
+				best = new HashSet<Esame>(parziale);
+				bestMedia = mediaVoti;
+			}
+			
+			return;
+		}
+		
+		//Sicuramente i crediti sono < m se siamo in questo punto
+		if(l == esami.size())
+			return;
+		
+		//Proviamo ad aggiungere esam[l]
+		parziale.add(esami.get(l));
+		cerca2(parziale, l+1, m);
+		
+		//Proviamo a NON aggiumgere esami[l]
+		parziale.remove(esami.get(l));
+		cerca2(parziale, l+1, m);
+		
+	}
 	
 	public double calcolaMedia(Set<Esame> esami) {
 		
